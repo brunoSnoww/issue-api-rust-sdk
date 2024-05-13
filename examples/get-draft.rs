5c9a226d09c4c852f10450928025eb41c36eb339
Bruno Neves <bruno.neves@issuu.com>
2024-05-13 18:04:17 -0300

    initial commit

--- examples/get-draft.rs
+++ examples/get-draft.rs
@@ -9,6 +9,8 @@ async fn main() {
     let slug = "o3xise3e481";
 
     let api = Api::new("https://api.issuu.com/v2/");
+    let token = "ory_at_og4PE9B1DsaXmsDWS2_qiuB_n9qIZTj8qFtDKQ7fCAw.N9ptXh7haA5wVRTQTcd8arraujLbVq46aHESJp8WgH0";
+    api.set_token(&format!("{token}")).await;
 
     let draft = Draft::new(api);
     let p = draft.get_draft(slug).await;
